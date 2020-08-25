f013dae38b2a044f767b218a88b64845028d8213
Murthy Vedula <mvedula@quicklogic.com>
2020-08-25 13:42:21 -0700

    converted s3-gateware folder to submodule

--- s3-gateware/gateware.h
+++
@@ -1,8 +0,0 @@
-#ifndef GATEWARE_H
-#define GATEWARE_H
-
-#include <stdint.h>
-extern uint32_t axFPGABitStream[];
-extern int      axFPGABitStream_length; // length of axFPGABitStream array in bytes
-
-#endif /* GATEWARE_H */
--- s3-gateware/usb2serial/src/usb2serial.c
+++
@@ -1,5 +0,0 @@
-#include <stdint.h>
-#include "usb2serial_bit.h"
-
-// length of axFPGABitStream array in bytes
-int   axFPGABitStream_length = sizeof(axFPGABitStream);
