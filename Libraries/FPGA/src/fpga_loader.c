2a15aee1ded7e3ccd730faf1e56840f5856d42c6
coolbreeze413 <coolbreeze413@yahoo.co.in>
2020-09-30 23:12:49 +0530

    add register settings as per jlink, and add readback verify step

--- Libraries/FPGA/src/fpga_loader.c
+++ Libraries/FPGA/src/fpga_loader.c
@@ -49,6 +49,7 @@
 #define REG15                           (*(volatile uint32_t *)(0x40004040))
 #define REG16                           (*(volatile uint32_t *)(0x40004048))
 #define REG17                           (*(volatile uint32_t *)(0x4000404C))
+#define REG18                           (*(volatile uint32_t *)(0x40000300))
 
 #define CFG_CTL_CFG_DATA                (*(volatile uint32_t *)(0x40014FFC))
 #define CFG_CTL_CFG_CTL                 (*(volatile uint32_t *)(0x40014000))
@@ -60,12 +61,13 @@
  *************************************************************/
 
 
-int verify_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr)
-{
 
+
+int load_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr)
+{
     uint32_t current_index = 0;
     uint32_t ram_content_size_in_words = 0;
-    uint32_t* current_block_addr = 0;
+    volatile uint32_t* current_block_addr = 0;
     uint32_t current_block_size_in_words = 0;
     uint32_t current_block_iterator = 0;
     uint8_t ram_block_num = 0;
@@ -91,7 +93,8 @@ int verify_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr
     {
         printf("no content to init ram blocks\r\n");
         return 0;
-    }
+    }   
+    
     
     // calculate the total number of entries in the array
     ram_content_size_in_words = ram_content_size/4;
@@ -113,9 +116,14 @@ int verify_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr
         // read the next "block_size_in_words" entries and write value into consecutive addresses
         for(current_block_iterator = 0; current_block_iterator < current_block_size_in_words; current_block_iterator++)
         {
-            if(*(current_block_addr) != ram_content_ptr[current_index + current_block_iterator])
+            *(current_block_addr) = ram_content_ptr[current_index + current_block_iterator];
+            // readback-verify
+            if(*current_block_addr != ram_content_ptr[current_index + current_block_iterator])
             {
-                printf("0x%08x\r\n", *(current_block_addr));
+                printf("addr: 0x%08x, read: 0x%08x, write: 0x%08x\r\n", current_block_addr,
+                                                                        *current_block_addr,
+                                                                        ram_content_ptr[current_index + current_block_iterator]);
+                                                                        
             }
             current_block_addr++;
         }
@@ -133,83 +141,7 @@ int verify_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr
        // otherwise, move to the next block, current_index is already at the right place.
        ram_block_num++;
     }
-
-    return 1;
     
-}
-
-
-int load_fpga_ram_content(uint32_t ram_content_size, uint32_t* ram_content_ptr)
-{
-    uint32_t current_index = 0;
-    uint32_t ram_content_size_in_words = 0;
-    uint32_t* current_block_addr = 0;
-    uint32_t current_block_size_in_words = 0;
-    uint32_t current_block_iterator = 0;
-    uint8_t ram_block_num = 0;
-    // the ram content initialization array is of the form:
-    //    uint32_t   axFPGAMemInit[] = {
-    //          [0]start_addr1, [1]size_in_bytes1,
-    //          [2 ... ] values1 (size_in_bytes1/4)...
-    //          start_addr2, size_in_bytes2,
-    //          values2 (size_in_bytes2/4) ...
-    // and so on.
-    
-    // so, we do:
-    // each ram block is defined by : 1 word addr, 1 word size_in_words
-    // and size_in_bytes next words of values
-    // read the block address and block size
-    // read the next "block_size_in_words" entries from the array and write to consecutive addresses
-    // starting from the block address
-    // check if we have reached end of the array, then done.
-    // othewise, we process the next ram block in the same way as above.
-    
-    // check if there is any ram blocks to init
-    if (ram_content_size == 0) 
-    {
-        printf("no content to init ram blocks\r\n");
-        return 0;
-    }
-    
-    // calculate the total number of entries in the array
-    ram_content_size_in_words = ram_content_size/4;
-    
-    printf("ram init content size: 0x%08x, %u\r\n", ram_content_size_in_words, ram_content_size_in_words);
-        
-    
-    // process all ram blocks
-    while(1)
-    {
-        // read block_addr and block_size_in_words
-        current_block_addr = (uint32_t *)ram_content_ptr[current_index++];
-        current_block_size_in_words = ram_content_ptr[current_index++];
-        printf("ram_block: [%d], start:[0x%08x], size: [0x%08x, %u]\r\n", ram_block_num,
-                                                                     current_block_addr,
-                                                                     current_block_size_in_words,
-                                                                     current_block_size_in_words);
-                                                                     
-        // read the next "block_size_in_words" entries and write value into consecutive addresses
-        for(current_block_iterator = 0; current_block_iterator < current_block_size_in_words; current_block_iterator++)
-        {
-            *(current_block_addr++) = ram_content_ptr[current_index + current_block_iterator];
-        }
-        
-        // increment the index with the current block size
-        current_index += current_block_size_in_words;
-        
-       // check if any more ram blocks remaining:
-       if(current_index >= ram_content_size_in_words) 
-       {
-            printf("end of ram blocks initialization\r\n");
-            break;
-       }
-       
-       // otherwise, move to the next block, current_index is already at the right place.
-       ram_block_num++;
-    }
-    
-    verify_fpga_ram_content(ram_content_size, ram_content_ptr);
-
     return 1;
 }
 
@@ -266,6 +198,28 @@ int load_fpga(uint32_t image_size, uint32_t* image_ptr, uint32_t ram_content_siz
 	}
 
 	CFG_CTL_CFG_CTL = 0x0; // exit config mode
+	
+	// required wait time
+	for (i=0;i<5000; i++) {
+		PMU->GEN_PURPOSE_1  = i << 4;
+	}
+	
+	REG18 = 0x1; // APB mode
+	
+	// required wait time 
+	for (i=0;i<5000; i++) {
+		PMU->GEN_PURPOSE_1  = i << 4;
+	}
+	
+	load_fpga_ram_content(ram_content_size, ram_content_ptr);
+	
+	REG18 = 0x0; // exit APB mode
+	
+	// required wait time 
+	for (i=0;i<500; i++) {
+		PMU->GEN_PURPOSE_1  = i << 4;
+	}
+	
 	REG10 = 0;
 
 
@@ -291,7 +245,7 @@ int load_fpga(uint32_t image_size, uint32_t* image_ptr, uint32_t ram_content_siz
 
 	//printf("FPGA is programmed\r\n");
 	
-	load_fpga_ram_content(ram_content_size, ram_content_ptr);
+	
 
 	return 1;
 
