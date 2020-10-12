33e70efbfb92130a64df9b5582ccb1022fb66739
coolbreeze413 <coolbreeze413@yahoo.co.in>
2020-10-12 19:57:08 +0530

    remove qf_ramblockinit test app from PR branch. commit this to separate tests repo once that is decided

--- qf_testapps/qf_ramblockinit/src/fpga_design.c
+++
@@ -1,7 +0,0 @@
-
-#include <stdint.h>
-#include "top_bit.h"   // FPGA bitstream to load into FPGA
-
-
-uint32_t axFPGABitStream_length = sizeof(axFPGABitStream);
-uint32_t axFPGAMemInit_length = sizeof(axFPGAMemInit);
