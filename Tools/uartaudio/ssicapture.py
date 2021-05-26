12c2941cf9274757f3345091e23fbe03c3bdba07
Murthy Vedula <mvedula@quicklogic.com>
2021-05-26 15:34:32 -0700

    some fixes to ssicapture script

--- Tools/uartaudio/ssicapture.py
+++ Tools/uartaudio/ssicapture.py
@@ -1,17 +1,27 @@
-"""Simple streaming audio data capture over UART
+"""Simple streaming audio data capture over UART or decode raw SSI data file
 
 This script capture audio data over UART (or USB2Serial) from Quickfeather 
 running simple streaming application project (qf_ssi_ai_app). 
 
-usage: getaudio.py [-h] [--port COMPORT] [--out AUDIO_FILE]
+This script also provides an offline decoder to decode a raw SSI data file 
+in SSI enhanced protocol
+
+usage: ssicapture.py [-h] [infile | --port COMPORT] [--out AUDIO_FILE]
+                     [--txport TXPORT] [--rawdata rawfile] [--payload length]
+                     [infile]
 
 Capture audio data from SSI App
 
 optional arguments:
   -h, --help        show this help message and exit
-  --port COMPORT    COM port, default is COM5
+  --port COMPORT    COM port, default is None
+  --baud baudrate   COM port baudrate (default is 460800)
   --out AUDIO_FILE  Audio filename, default is audio.bin
-
+  --txport TXPORT   COM port to send connect/disconnect commands
+  --rawdata rawfile save raw data to rawfile
+  --payload length  expected payload length in each packet, 
+                    default is 224 bytes
+  infile            Input file to decode
 """
 
 import serial
@@ -19,24 +29,28 @@ import argparse
 import time
 import struct
 import itertools
+import logging
+import sys
 
 parser = argparse.ArgumentParser(description="Capture audio data from SSI App")
 parser.add_argument('infile', nargs='?', type=argparse.FileType('rb'), default=None)
 parser.add_argument('--port', dest='comport', default=None, type=str,
-                    help='COM port, default is COM5')
+                    help='capture data from COM port')
+parser.add_argument('--baud', dest='baudrate', default=115200, type=int,
+                    help='baudrate, default is 115200')
 parser.add_argument('--txport', dest='txport', default=None, type=str)
 parser.add_argument('--out', dest='audio_file', default='audio.bin', type=str,
                     help='Audio filename, default is audio.bin')
 parser.add_argument('--rawdata', dest='rawdata', default=None, type=str,
-                    help='raw data filename, default is rawdata.bin')
-parser.add_argument('--payload', dest='expected_dlen', default=224, type=int, 
+                    help='raw data filename')
+parser.add_argument('--payload', dest='expected_dlen', default=480, type=int, 
                      help='Expected payload length')
 
-def decodessiv2(serbytes, payload_length=224):
+def decodessiv2(serbytes, payload_length=480):
     decodessiv2.seqnum = getattr(decodessiv2, 'seqnum', 0)
     decodessiv2.sync_data = getattr(decodessiv2, 'sync_data', b'\xFF')
     decoded_bytes = b''
-    #print('serial bytes = ', serbytes)
+    #print('in  = ', serbytes)
     while (len(serbytes) >= 8):
       # scan for sync byte
       indx = serbytes.find( decodessiv2.sync_data )
@@ -59,30 +73,33 @@ def decodessiv2(serbytes, payload_length=224):
          for b in bytes_data:
            cs = cs ^ b
          if (cs == checksum):
-           #outbytes = outbytes + dlen
-           #packet_count = packet_count + 1
-           return textstr, serbytes[indx+8:]
+           #print('out = ', textstr)
+           return textstr, serbytes[indx+8+dlen:]
          else:
-           serbytes = serbytes[(indx+8):]
-           print('checksum [{}, {}] error at packet sequence number {}'.format(checksum, cs, decodessiv2.seqnum))
+           serbytes = serbytes[(indx+8+dlen):]
+           #print('checksum [{}, {}] error at packet sequence number {}'.format(checksum, cs, decodessiv2.seqnum))
+           logging.error('checksum [%d, %d] error at packet sequence number %d', checksum, cs, decodessiv2.seqnum)
       else:
+         logging.error('sync [%d, %d, %d] error at packet sequence number %d', sync, seqnum, dlen, decodessiv2.seqnum)
          serbytes = serbytes[1:]
     return decoded_bytes, serbytes
 
 args = parser.parse_args()
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, 
+                    format='%(asctime)s %(message)s', datefmt='%Y-%d-%m %I:%M:%S %p')
 print(args)
-sample_count=0
 samples_per_sec = 0
 samples_secs_length = 0
 syncString = b'AUDIOSTREAMSTART'
 
 if (args.comport != None):
   try:
-    ser = serial.Serial(args.comport, 46080, timeout=1)
+    ser = serial.Serial(args.comport, args.baudrate, timeout=1)
+    logging.info('Opened COMPORT %s', args.comport)
     if (args.txport == None):
       sertx = ser
     else:
-      sertx = serial.Serial(args.txport, 115200, timeout=1)
+      sertx = serial.Serial(args.txport, args.baudrate, timeout=1)
   except:
     print('Error opening port ', args.comport)
     raise SystemExit(1)
@@ -97,10 +114,13 @@ if (args.rawdata != None):
    rawdatafd = open(args.rawdata, "wb")
 bytes_in = 0
 bytes_out = 0
+bytes_dropped = 0
+connected = False
 with open(args.audio_file, 'wb') as af:
    if (args.comport):
       ser.reset_input_buffer()
       sertx.write(b'connect')
+      logging.info('Connect request sent')
    start = time.time()
    currstr = b''
    while True:
@@ -108,7 +128,7 @@ with open(args.audio_file, 'wb') as af:
        l = len(currstr)
        r = 512 - l
        nextstr = ser.read(r) # read upto 512 bytes
-       if (len(nextstr) == 0):
+       if ((args.comport == None) and (args.infile != None) and len(nextstr) == 0):
           break
        bytes_in = bytes_in + len(nextstr)
        if (rawdatafd != None):
@@ -116,20 +136,25 @@ with open(args.audio_file, 'wb') as af:
        nextstr = currstr + nextstr
        out, currstr = decodessiv2(nextstr, payload_length=args.expected_dlen)
        bytes_out = bytes_out + len(out)
+       if (bytes_out > 0) and (not connected):
+          connected = True
+          logging.info('Connected')
        if (len(out) > 0):
           af.write(out)
-       sample_count = sample_count + len(nextstr)
        samples_per_sec = samples_per_sec + len(nextstr)
        if time.time() - start > 1:
           start = time.time()
           samples_per_sec = 0
           samples_secs_length = samples_secs_length + 1
        if (samples_per_sec == 0) and ( samples_secs_length % 60 == 1 ):
-          print('bytes in = ', bytes_in, 'out = ', bytes_out, 
-               'samples = ', sample_count, samples_secs_length//60, ' mins')
+          bytes_dropped = (args.expected_dlen*bytes_in) // (args.expected_dlen+8)
+          bytes_dropped = bytes_dropped - bytes_out
+          logging.info('bytes in = %d, out = %d, dropped = %d', bytes_in, bytes_out, bytes_dropped)
      except KeyboardInterrupt:
-       print('{} samples written to {}'.format(sample_count, args.audio_file))
        if (args.comport):
          sertx.write(b'disconnect')
+       bytes_dropped = (args.expected_dlen*bytes_in) // (args.expected_dlen+8)
+       bytes_dropped = bytes_dropped - bytes_out
+       logging.info('bytes in = %d, out = %d, dropped = %d', bytes_in, bytes_out, bytes_dropped)
        break
 
