bd1d660aa40b7ef7bb8ebd5d377a07d89e2646d1
murthyvedula <66380686+murthyvedula@users.noreply.github.com>
2021-05-26 10:45:36 -0700

    qf_vr_i2s_app build issue fix (#125)

--- qf_vr_apps/qf_vr_i2s_app/src/main.c
+++ qf_vr_apps/qf_vr_i2s_app/src/main.c
@@ -58,7 +58,7 @@
 #include "host_interface.h"
 #include "d2h_protocol.h"
 
-#include "qf_hardwareSetup.h"
+#include "qf_hardwaresetup.h"
 #include "eoss3_hal_pad_config.h"
 #if (FEATURE_FLL_I2S_DEVICE == 1)
 #include "fpga_loader.h"
