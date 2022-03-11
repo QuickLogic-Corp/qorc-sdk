0a9b39bd2e8b293660020be0797da1580306f237
Murthy Vedula <mvedula@quicklogic.com>
2022-03-11 13:06:46 -0800

    Add multiple app loading support for booatloader over UART

--- qf_apps/qf_bootloader_uart/src/bootloader_task.c
+++ qf_apps/qf_bootloader_uart/src/bootloader_task.c
@@ -48,14 +48,19 @@ int check_active_images(void);
 #define BLUE_LED_GPIO_NUM         (4) //PAD 18, GPIO is connected to Blue LED
 #define GREEN_LED_GPIO_NUM        (5) //PAD 21, GPIO is connected to Green LED
 #define RED_LED_GPIO_NUM          (6) //PAD 22, GPIO is connected to Red LED
-
+#define M4APP_SELECTION_GPIO_NUM  (2) //PAD 11, (TBD) placeholder
+#define M4APP_SELECTION_GPIO_NUM2 (3) //PAD 12?, (TBD) placeholder
 
 #define METADATA_UNKNOWN_FLASH_STATE        (0x0)
 #define METADATA_APPFPGA_IS_FLASHED         (0x1)
 #define METADATA_M4APP_IS_FLASHED           (0x2)
 #define METADATA_APPFFE_IS_FLASHED          (0x4)
+#define METADATA_M4APP2_IS_FLASHED          (0x8)
+#define METADATA_M4APP3_IS_FLASHED          (0x10)
+#define METADATA_M4APP4_IS_FLASHED          (0x20)
 #define METADATA_VALID_IMAGE_IS_FLASHED     (METADATA_APPFPGA_IS_FLASHED|METADATA_M4APP_IS_FLASHED) // ffe not supported yet
 
+uint8_t g_m4app_selection_gpio_value[2] ;
 TaskHandle_t 	BLTaskHandle;
 static int user_button_pressed = 0;
 //static int user_btn_on_count = 0;
@@ -193,6 +198,9 @@ static void BLTaskHandler(void *pvParameters)
       // green led indicates waiting for button press
       toggle_waiting_led(200);
       
+      HAL_GPIO_Read(M4APP_SELECTION_GPIO_NUM, &g_m4app_selection_gpio_value[0]);
+      g_m4app_selection_gpio_value[1] = 0; // default value
+      //HAL_GPIO_Read(M4APP_SELECTION_GPIO_NUM2, &g_m4app_selection_gpio_value[1]);
       //if User button is pressed load USB FPGA image
       check_user_button();
       if(user_button_pressed)
@@ -280,6 +288,21 @@ static void BLTaskHandler(void *pvParameters)
 	}
 }
 
+typedef struct st_active_image_info {
+	uint32_t image_meta_address;
+	uint32_t image_meta_size;
+	uint8_t  is_flashed;
+} active_image_info_t;
+
+active_image_info_t active_image_info[]=
+{
+   { FLASH_APPFPGA_META_ADDRESS, FLASH_APPFPGA_META_SIZE, METADATA_APPFPGA_IS_FLASHED },
+   { FLASH_APPFFE_META_ADDRESS,  FLASH_APPFFE_META_SIZE,  METADATA_APPFFE_IS_FLASHED },
+   { FLASH_APP_META_ADDRESS,     FLASH_APP_META_SIZE,     METADATA_M4APP_IS_FLASHED },
+   { FLASH_APP2_META_ADDRESS,    FLASH_APP2_META_SIZE,    METADATA_M4APP2_IS_FLASHED },
+   { FLASH_APP3_META_ADDRESS,    FLASH_APP3_META_SIZE,    METADATA_M4APP3_IS_FLASHED },
+   { FLASH_APP4_META_ADDRESS,    FLASH_APP4_META_SIZE,    METADATA_M4APP4_IS_FLASHED },
+};
 int check_active_images()
 {
     uint8_t active_images = 0x0;
@@ -295,6 +318,36 @@ int check_active_images()
     // perhaps we can move the crc check here itself, rather than in the loaders?
     // for now, I think our organization is ok.
 
+    int num_images = sizeof(active_image_info)/sizeof(active_image_info[0]);
+    for (int k = 0; k < num_images; k++)
+    {
+    	uint32_t meta_address = active_image_info[k].image_meta_address;
+    	uint32_t meta_size    = active_image_info[k].image_meta_size;
+    	uint8_t  is_flashed   = active_image_info[k].is_flashed;
+        // read the fpga metadata
+        bufPtr = (unsigned char *)image_metadata;
+        read_flash((unsigned char *)meta_address, meta_size, bufPtr);
+        image_crc = image_metadata[0];
+        image_size = image_metadata[1];
+        image_info = image_metadata[2];
+        image_info_ptr8 = (uint8_t*)(&image_info);
+        //dbg_str("fpga: 0x");dbg_hex8(image_info_ptr8[0]);dbg_str("\r\n");
+        if(image_info_ptr8[0] == 0x03)
+        {
+            if(image_size == 0xFFFFFFFF || image_crc == 0xFFFFFFFF)
+            {
+                // error, image is marked active, but doesn't seem to have image flashed!
+                dbg_str("warning: appfpga image marked active, but seems to have invalid image\r\n");
+            }
+            else
+            {
+                // ok, active image, and looks to have a proper image
+                active_images |= is_flashed;
+            }
+
+        }
+
+    }
     // read the fpga metadata
     bufPtr = (unsigned char *)image_metadata; 
     read_flash((unsigned char *)FLASH_APPFPGA_META_ADDRESS, FLASH_APPFPGA_META_SIZE, bufPtr);
@@ -317,7 +370,8 @@ int check_active_images()
         }
         
     }
-
+    return active_images;
+#if (0)
     // read the ffe metadata
     bufPtr = (unsigned char *)image_metadata; 
     read_flash((unsigned char *)FLASH_APPFFE_META_ADDRESS, FLASH_APPFFE_META_SIZE, bufPtr);
@@ -365,6 +419,7 @@ int check_active_images()
     }
 
     return active_images;
+#endif
 }
 
 /*!
