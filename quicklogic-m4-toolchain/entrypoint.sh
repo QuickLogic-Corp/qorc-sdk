ef7fe3328366c58971e8493c763c9375e3a992ac
Chris Knorowski <cknorow@gmail.com>
2023-06-08 15:03:34 -0700

    Adds Dockerfile for building QF and QT code for the M4 app (#154)
    * adds dockerfile for building qf code
    
    * update readme
    
    * update readme
    
    ---------
    
    Co-authored-by: Chris Knorowski <chris.knorowski@sensiml.com>

---
+++ quicklogic-m4-toolchain/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+case "$1" in
+   build)
+   
+   cd $2/GCC_Project 
+   make -j
+   ;;
+   clean)
+   
+   cd $2/GCC_Project 
+   make clean
+   ;;
+  *)
+    echo -e "## Commands"
+    echo -e '\n    build <path-to-app>: build the application'
+    echo -e '\n    clean <path-to-app>: clean the application'    
+    echo -e "\n\n## Running docker container"
+    echo -e "\nThe qorc-sdk needs to be mounted to the volume qorc-sdk in the docker container "
+    echo -e '\n    docker run -v "<absolute-path-to-qorc-sdk-on-host>:/qorc-sdk" build <path-to-app>'
+    echo -e '\n    docker run -v "<absolute-path-to-qorc-sdk-on-host>:/qorc-sdk" clean <path-to-app>'
+    echo -e "\nNote: <path-to-app> should start with /qorc-sdk/"
+    echo -e "\n## Example build"
+    echo -e '\n   docker run -v "<absolute-path-to-qorc-sdk-on-host>:/qorc-sdk" build /qorc-sdk/qf_apps/qf_ssi_ai_app\n\n'
+    exec "$@"
+    ;;
+esac
